d8cd20787077e9ea876a82ed506d6cf616c4effd
Lovepreet Singh <dev.lovepreetsingh@gmail.com>
2016-07-08 00:53:40 +0530

    added code for using realm database as alternative for sqlite

---
+++ RealmDatabase/app/src/main/java/com/devlovepreet/realmdatabase/adapters/RealmBooksAdapter.java
@@ -0,0 +1,14 @@
+package com.devlovepreet.realmdatabase.adapters;
+
+import android.content.Context;
+
+import com.devlovepreet.realmdatabase.model.Book;
+import io.realm.RealmResults;
+
+public class RealmBooksAdapter extends RealmModelAdapter<Book> {
+
+    public RealmBooksAdapter(Context context, RealmResults<Book> realmResults, boolean automaticUpdate) {
+
+        super(context, realmResults, automaticUpdate);
+    }
+}
